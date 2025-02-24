5a1814b57e863167d1b8a113d9061bc947e748bd
cong <cong.li2716@gmail.com>
2025-02-24 21:51:57 +0100

    Add files via upload

---
+++ Activity 3/PAMmodulator_2023.m
@@ -0,0 +1,31 @@
+% ************************************************************************
+% Author: CONG LI
+% Date Created: 2023
+%
+% This script is part of a course project for 
+% Communication Systems: Theory and Measurement M
+% at University of Bologna.
+% ************************************************************************
+function [x,h,Delay]=PAMmodulator_2023(symbols,nsps,Nf,pulsetype,rolloff)
+% symbols: input symbols
+% nsps: samples per symbol time
+% Nf: number of filter coefficients
+% pulsetype: 'RECT' rectangular NRZ pulse, 'ROOTRAISEDCOSINE' root raised cosine pulse
+% rolloff: roll-off factor for root raised cosine pulses
+N=length(symbols); % number of symbols
+M=N*nsps; % number of samples of the PAM signal
+xz= zeros(1,M) ; % Generate a row vector filled with zeros, with length M
+xz(1:nsps:M)=symbols; % copy the symbols into xz
+switch pulsetype
+case 'ROOTRAISEDCOSINE'
+h = rcosdesign(rolloff,Nf/nsps,nsps,'sqrt'); % Root raised cosine pulse
+case 'RECT'
+Nf=nsps;
+h= ones(1,Nf); % Rectangular pulse
+otherwise
+h=1;
+disp('pulse not supported');
+end
+x= conv(xz,h); % filter the pulses
+Delay=(Nf-1)/2; % delay
+end
